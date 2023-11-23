ad408263fb60fdfb6f1a8db400e0dca7d75ae7f0
vas-web <134453434+vas-web@users.noreply.github.com>
2023-11-23 20:53:48 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Найти сумму n целых, случайных чисел (ДСЧ), распределенных в
+диапазоне от 0 до n и меньше n/2. Вывести на экран на одной
+строке сгенерированные числа, на другой строке результат.*/
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static int Summa(int n, out string values)
+        {
+            Random rnd = new Random();
+            int sum = 0;
+            values = string.Empty;
+            for (int i = 0 ; i < n ; i++)
+            {
+                int rand = rnd.Next(0, n);
+
+                if (rand < n/2)
+                {
+                    sum+=rand;
+                }
+                values += Convert.ToString(rand + " ; ");
+            }
+            return sum;
+
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void result_Click(object sender, RoutedEventArgs e)
+        {
+            string boba;
+            int boobs = Convert.ToInt32(textbox1.Text);
+            int biba = Class1.Summa(boobs,out boba);
+            textbox2.Text = boba;
+            textbox3.Text = Convert.ToString(biba);
+        }
+    }
+
+}
